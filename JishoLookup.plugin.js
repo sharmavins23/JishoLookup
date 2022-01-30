0aca63be12bbd10b5a96c0c9ff342dffb609f3ae
sharmavins23 <sharmavins23@gmail.com>
2022-01-30 00:47:33 -0500

    added initial JS

---
+++ JishoLookup.plugin.js
@@ -0,0 +1,25 @@
+/**
+ * @name JishoLookup
+ * @author sharmavins23
+ * @updateUrl https://raw.githubusercontent.com/sharmavins23/JishoLookup/JishoLookup.plugin.js
+ * @authorLink https://github.com/sharmavins23
+ * @source https://github.com/sharmavins23/JishoLookup/JishoLookup.plugin.js
+ */
+
+const config = {
+    info: {
+        name: "JishoLookup",
+        authors: [
+            {
+                name: "sharmavins23",
+                discord_id: "236964881213947914",
+                github_username: "sharmavins23",
+            },
+        ],
+        version: "1.0.0",
+        description:
+            "Search for words on Jisho.org. Select a word, right click and press Jisho to see its definition.",
+        github_raw:
+            "https://raw.githubusercontent.com/sharmavins23/JishoLookup/JishoLookup.plugin.js",
+    },
+};
